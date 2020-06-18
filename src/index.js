263fbe734d41306be9d6a645f95032930100d839
Mohamad Ilham Ramadhan <ilhamzoldyck13@gmail.com>
2020-06-18 11:46:11 +0700

    setting webpack, babel, fonts, telah selesai

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+import "./styles/style.scss";
+
+const App = () => {
+  return <div>Persiapan telah selesai.</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
